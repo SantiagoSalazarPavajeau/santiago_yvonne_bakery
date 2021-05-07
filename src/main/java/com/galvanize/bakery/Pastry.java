3dbdc3bb6ca2d2460b062b04bcd41f15e1991266
Yvonne <yvonnersz@gmail.com>
2021-05-07 10:12:25 -0700

    getIngredients method returns Array list of ingredients

--- src/main/java/com/galvanize/bakery/Pastry.java
+++ src/main/java/com/galvanize/bakery/Pastry.java
@@ -1,14 +1,22 @@
 package com.galvanize.bakery;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pastry {
     private String name;
     private double price;
     private boolean glutenFree;
+    private List<String> ingredients = new ArrayList<>();
 
     Pastry(String name, double price, boolean glutenFree){
         this.name = name;
         this.price = price;
         this.glutenFree = glutenFree;
+    }
+
+    public List<String> getIngredients(){
+        return ingredients;
     }
 
 
--- src/test/java/com/galvanize/bakery/PastryTests.java
+++ src/test/java/com/galvanize/bakery/PastryTests.java
@@ -1,19 +1,39 @@
 package com.galvanize.bakery;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PastryTests {
     // name, price, gluten-free, ingredients
+    Pastry pastry;
+
+    @BeforeEach
+    void setUp(){
+        pastry = new Pastry("Cheese cake", 5.00, false);
+    }
 
     @Test
     public void initializePastryWithFields(){
-        Pastry pastry = new Pastry("Cheese cake", 5.00, false);
-
-        assertTrue(pastry instanceof Pastry, "Should be able to initialize a pastry with fields: name, price, gluten-free");
+        assertNotNull(pastry, "Should be able to initialize a pastry with fields: name, price, gluten-free");
     }
+
+    @Test
+    public void getIngredientsExistsOnPastry() {
+        assertTrue(pastry.getIngredients() instanceof List, "getIngredients method returns Array list of ingredients");
+    }
+
+//    @Test
+//    public void addIngredientsToPastry(){
+//        pastry.addIngredient("cheese");
+//        pastry.addIngredient("strawberries");
+//        pastry.addIngredient("sugar");
+//
+//        assertEquals(3, pastry.getIngredients().size());
+//    }
+
 }
