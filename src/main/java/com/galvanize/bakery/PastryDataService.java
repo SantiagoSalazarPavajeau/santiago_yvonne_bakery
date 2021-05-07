1de7d0d950fbbb728e0edd0a94e71c2e1b0638ab
Yvonne <yvonnersz@gmail.com>
2021-05-07 10:49:31 -0700

    Creates addData(), passes in pastry and returns passed argument

---
+++ src/main/java/com/galvanize/bakery/PastryDataService.java
@@ -0,0 +1,13 @@
+package com.galvanize.bakery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PastryDataService {
+    private List<Pastry> pastryList = new ArrayList<>();
+
+    public Pastry addToMyData(Pastry pastry) {
+        pastryList.add(pastry);
+        return pastry;
+    }
+}
---
+++ src/test/java/com/galvanize/bakery/PastryServiceTests.java
@@ -0,0 +1,37 @@
+package com.galvanize.bakery;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+
+public class PastryServiceTests {
+    PastryDataService pastryDataService;
+    List<Pastry> pastryData;
+
+    @BeforeEach
+    void setUp() {
+        pastryData = new ArrayList<>();
+        pastryDataService = new PastryDataService();
+
+        pastryData.add(pastryDataService.addToMyData(new Pastry("Cheese cake", 5.00, false)));
+        pastryData.add(pastryDataService.addToMyData(new Pastry("Strawberry shortcake", 5.50, false)));
+        pastryData.add(pastryDataService.addToMyData(new Pastry("Carrot cake", 4.75, false)));
+
+    }
+
+    @Test
+    public void createAddDataMethodReturnsData() {
+        Pastry pastry = new Pastry("Cheese cake", 5.00, false);
+        Pastry actual = pastryDataService.addToMyData(pastry);
+
+        assertEquals(pastry, actual, "Creates addData method and passes in pastry and returns the passed argument");
+    }
+}
